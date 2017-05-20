d03fd8cedca9ca8255a3e53d603e411c6879cf0f
Christian Mehlmauer <firefart@gmail.com>
2017-05-20 10:44:50 +0200

    more work

--- query_db.sh
+++ query_db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-psql -e -q -x -c "SELECT block.inetnum, block.country, block.description, block.maintained_by, block.created, block.last_modified, block.source FROM block WHERE block.inetnum >> '$1' ORDER BY block.inetnum DESC;" ripe
+psql -e -q -x -c "SELECT block.inetnum, block.netname, block.country, block.description, block.maintained_by, block.created, block.last_modified, block.source FROM block WHERE block.inetnum >> '$1' ORDER BY block.inetnum DESC;" ripe
