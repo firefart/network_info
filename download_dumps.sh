5ec7359dea9223639b10e616f005063fe7a3cb4b
Christian Mehlmauer <firefart@gmail.com>
2017-05-20 09:42:33 +0200

    fix errors

--- download_dumps.sh
+++ download_dumps.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-mkdir -p ./databases
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DOWNLOAD_DIR="$DIR/databases"
+mkdir -p $DOWNLOAD_DIR
 
-wget ftp://ftp.afrinic.net/pub/dbase/afrinic.db.gz
+function download {
+  name=$(echo $1 |awk -F "/" '{print $NF}')
+  echo "Downloading $name..."
+  wget -O "$DOWNLOAD_DIR/$name" "$1"
+}
 
-wget ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inetnum.gz
-wget ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inet6num.gz
+download "ftp://ftp.afrinic.net/pub/dbase/afrinic.db.gz"
 
-wget ftp://ftp.arin.net/pub/rr/arin.db
+download "ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inetnum.gz"
+download "ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inet6num.gz"
 
-wget ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest
+download "ftp://ftp.arin.net/pub/rr/arin.db"
 
-wget ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz
-wget ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inet6num.gz
+download "ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest"
+
+download "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz"
+download "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inet6num.gz"
